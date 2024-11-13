8b1b7e50642c9f2afe8370c44e747882e7067e90
Lucas de Souza Santos <lucass.santoss210@gmail.com>
2024-11-13 10:11:31 -0300

    terminando as operações grud do user

---
+++ PythonBackEndDevoloper/flask-project/anotacoes/04_metodos_http.md
@@ -0,0 +1,4 @@
+# Métodos HTTP
+
+- PUT: é para quando eu quero atualizar o objeto inteiro 
+- PATCH: é para quando eu quero atualizar o objeto parcialmente.
--- PythonBackEndDevoloper/flask-project/src/controllers/user.py
+++ PythonBackEndDevoloper/flask-project/src/controllers/user.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request
 from src.app import User, db
 from http import HTTPStatus
+from sqlalchemy import inspect
 
 app = Blueprint("user", __name__, url_prefix="/users")
 
@@ -31,3 +32,38 @@ def handle_user():
         return {"message": "User created!"}, HTTPStatus.CREATED
     else:
         return {'users': _list_users()}
+
+
+@app.route("/<int:user_id>")
+def get_user(user_id):
+    user = db.get_or_404(User, user_id)
+    return {
+        "id": user.id,
+        "username": user.username
+    }
+
+
+@app.route("/<int:user_id>", methods=["PATCH", "PUT"])
+def update_user(user_id):
+    user = db.get_or_404(User, user_id)
+    data = request.json
+
+    mapper = inspect(User)
+    for column in mapper.attrs:
+        if column.key in data:
+            setattr(user, column.key, data[column.key])
+    db.session.commit()
+
+    return {
+        "id": user.id,
+        "username": user.username
+    }
+
+
+@app.route("/<int:user_id>", methods=["DELETE"])
+def remove_user(user_id):
+    user = db.get_or_404(User, user_id)
+    db.session.delete(user)
+    db.session.commit()
+
+    return "", HTTPStatus.NO_CONTENT
