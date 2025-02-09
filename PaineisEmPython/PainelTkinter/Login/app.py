fe2c9e9826eb911c15fae2acfafbc560e2923b4e
Lucas de Souza Santos <lucass.santoss210@gmail.com>
2025-02-09 10:14:03 -0300

    inicio do projeto de Login com Python

---
+++ PaineisEmPython/PainelTkinter/Login/app.py
@@ -0,0 +1,12 @@
+import customtkinter  # Import the module
+
+
+customtkinter.set_appearance_mode("dark")
+# customtkinter.set_default_theme("dark-blue")
+
+janela = customtkinter.CTk()
+janela.geometry("400x400")
+janela.title("Custom Tkinter")
+
+
+janela.mainloop()
